b45f1178530c0d672b0d80a36f81d209885b71d9
progress-more <991414354@qq.com>
2019-12-19 09:15:29 +0800

    新建登录页面和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home.vue'
+import login from '../views/login.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/home' },
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // 由于spa首屏渲染时会将所有模板的代码都一次性加载到页面上造成首屏加载缓慢，需用
-    // 按需加载的引用方式，只有用到该组件的时候才会被加载到页面上，节省了多余的资源；
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/login', component: login }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // 由于spa首屏渲染时会将所有模板的代码都一次性加载到页面上造成首屏加载缓慢，需用
+  //   // 按需加载的引用方式，只有用到该组件的时候才会被加载到页面上，节省了多余的资源；
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
