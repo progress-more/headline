46c2a08d315a76de0a2d12be61a0a0bb96113219
progress-more <991414354@qq.com>
2019-12-20 16:22:53 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -7,25 +7,27 @@
       </div>
       <!-- 导航组件el-menu 默认垂直模式 mode属性设置水平垂直；
   submenu组件可生成二级菜单 menu还提供了bgc,text-color,active-text-color等样式-->
-    <el-menu background-color='#353b4e' text-color=' #adafb5' active-text-color=' #ffd04b'>
-        <el-menu-item>首页</el-menu-item>
+    <el-menu router background-color='#353b4e' text-color=' #adafb5' active-text-color=' #ffd04b'>
+        <!-- 导航点击需跳转相应页面 so在el-menu上添加router属性 开启路由;
+        启用该模式会在激活导航时以index为path进行路由跳转 -->
+        <el-menu-item index='/home'>首页</el-menu-item>
         <el-submenu index='1'>
             <!-- 具名插槽 -->
             <span slot="title">内容管理</span>
             <!-- 匿名插槽 -->
-            <el-menu-item>发布文章</el-menu-item>
-            <el-menu-item>文章列表</el-menu-item>
-            <el-menu-item>评论管理</el-menu-item>
-            <el-menu-item>素材管理</el-menu-item>
+            <el-menu-item index='/home/publish'>发布文章</el-menu-item>
+            <el-menu-item index='/home/articles'>文章列表</el-menu-item>
+            <el-menu-item index='/home/comment'>评论管理</el-menu-item>
+            <el-menu-item index='/home/material'>素材管理</el-menu-item>
         </el-submenu>
         <el-submenu index='2'>
             <!-- 具名插槽 -->
             <span slot="title">粉丝管理</span>
             <!-- 匿名插槽 -->
-            <el-menu-item>图文数据</el-menu-item>
-            <el-menu-item>粉丝概况</el-menu-item>
-            <el-menu-item>粉丝画像</el-menu-item>
-            <el-menu-item>粉丝列表</el-menu-item>
+            <el-menu-item index='/home/picture'>图文数据</el-menu-item>
+            <el-menu-item index='/home/fansinfo'>粉丝概况</el-menu-item>
+            <el-menu-item index='/home/fanspicture'>粉丝画像</el-menu-item>
+            <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
         </el-submenu>
         <el-menu-item>账户信息</el-menu-item>
     </el-menu>
