9fe7dd486177a877405668ea2ea73c7bd8ef2e2f
progress-more <991414354@qq.com>
2019-12-22 12:01:17 +0800

    面包屑组件

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--  创建一个面包屑组件  用于显示当前页面的路径 快速返回之前的任意页面-->
+  <el-breadcrumb separator='>'>
+      <el-breadcrumb-item to='/home'>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+          <!-- 第二个为动态显示 设置具名插槽-->
+          <slot name="title"></slot>
+      </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 // 将侧边栏与 头部等一般组件 进行全局注册
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
-
+import breadCrumb from './common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
