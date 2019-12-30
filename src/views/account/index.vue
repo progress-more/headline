a9cd7e9325bd7039cfd6ca76d15a0ead390fb077
progress-more <991414354@qq.com>
2019-12-30 20:27:37 +0800

    账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,32 @@
       <bread-crumb slot="header">
       <template slot="title">账户信息</template>
       </bread-crumb>
+      <el-row>
+          <el-col :span='12'>
+              <el-form label-width='100px'>
+                <el-form-item label='用户名'>
+                    <el-input style="width:50%"></el-input>
+                </el-form-item>
+                <el-form-item label='简介'>
+                    <el-input style="width:50%"></el-input>
+                </el-form-item>
+                <el-form-item label='邮箱'>
+                    <el-input style="width:50%"></el-input>
+                </el-form-item>
+                <el-form-item label='手机号'>
+                    <el-input disabled style="width:50%"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type='primary'>保存信息</el-button>
+                </el-form-item>
+              </el-form>
+          </el-col>
+          <el-col :span='12'>
+              <el-upload class="head-upload" action='' :show-file-list='false' :http-request='uploadImg'>
+                  <img src="../../assets/img/timg.jpg" alt="">
+              </el-upload>
+          </el-col>
+      </el-row>
   </el-card>
 </template>
 
@@ -12,6 +38,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scope>
+    .el-form {
+        margin-left: 100px;
+        .el-form-item {
+            padding: 12px 0;
+        }
+    }
+        .head-upload {
+            margin-top: 40px;
+            width: 300px;
+            height: 300px;
+            img {
+                width: 100%;
+                height: 100%;
+                 border-radius: 50%;
+            }
+        }
 </style>
