495c778e40a50e732484c2e14385f34487a05fa4
progress-more <991414354@qq.com>
2019-12-30 20:56:07 +0800

    账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -7,16 +7,16 @@
           <el-col :span='12'>
               <el-form label-width='100px'>
                 <el-form-item label='用户名'>
-                    <el-input style="width:50%"></el-input>
+                    <el-input v-model="formData.name" style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item label='简介'>
-                    <el-input style="width:50%"></el-input>
+                    <el-input v-model="formData.intro" style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item label='邮箱'>
-                    <el-input style="width:50%"></el-input>
+                    <el-input v-model="formData.email" style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item label='手机号'>
-                    <el-input disabled style="width:50%"></el-input>
+                    <el-input v-model="formData.mobile" disabled style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type='primary'>保存信息</el-button>
@@ -24,8 +24,8 @@
               </el-form>
           </el-col>
           <el-col :span='12'>
-              <el-upload class="head-upload" action='' :show-file-list='false' :http-request='uploadImg'>
-                  <img src="../../assets/img/timg.jpg" alt="">
+              <el-upload class="head-upload" action='' :show-file-list='false'>
+                  <img :src="formData.photo ? formData.photo : defaultImg" alt="">
               </el-upload>
           </el-col>
       </el-row>
@@ -33,8 +33,33 @@
 </template>
 
 <script>
+// 打开页面 获取用户信息 1.先定义变量接收返回数据 2.请求数据 3.渲染页面
 export default {
-
+  data () {
+    return {
+      formData: {
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像
+        email: '', //  邮箱
+        mobile: '' // 手机号
+      },
+      defaultImg: require('../../assets/img/timg.jpg')
+    }
+  },
+  methods: {
+    //   获取用户信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(res => {
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
