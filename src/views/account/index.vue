024d5ab090ebc25516e13e87f58c520a3601b8f5
progress-more <991414354@qq.com>
2019-12-30 21:15:20 +0800

    账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,21 +5,21 @@
       </bread-crumb>
       <el-row>
           <el-col :span='12'>
-              <el-form label-width='100px'>
-                <el-form-item label='用户名'>
+              <el-form ref="myForm" :model='formData' :rules='rules' label-width='100px'>
+                <el-form-item prop='name' label='用户名'>
                     <el-input v-model="formData.name" style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item label='简介'>
                     <el-input v-model="formData.intro" style="width:50%"></el-input>
                 </el-form-item>
-                <el-form-item label='邮箱'>
+                <el-form-item prop='email' label='邮箱'>
                     <el-input v-model="formData.email" style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item label='手机号'>
                     <el-input v-model="formData.mobile" disabled style="width:50%"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type='primary'>保存信息</el-button>
+                    <el-button @click='saveUserInfo' type='primary'>保存信息</el-button>
                 </el-form-item>
               </el-form>
           </el-col>
@@ -44,10 +44,32 @@ export default {
         email: '', //  邮箱
         mobile: '' // 手机号
       },
+      rules: {
+        name: [{ required: true, message: '用户名不能为空' },
+          { min: 1, max: 7, message: '用户名长度在1到7个字符' }],
+        email: [{ required: true, message: '邮箱不能为空' },
+          { pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/, message: '邮箱格式不正确' }]
+      },
       defaultImg: require('../../assets/img/timg.jpg')
     }
   },
   methods: {
+    //   保存用户信息
+    saveUserInfo () {
+      this.$refs.myForm.validate().then(res => {
+        // 若校验成功 请求保存
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(res => {
+          this.$message({
+            type: 'success',
+            message: '保存用户信息成功'
+          })
+        })
+      })
+    },
     //   获取用户信息
     getUserInfo () {
       this.$axios({
